246b5f89d71cd80accfacdee8be42eb7c7335d07
Mystic-Mirage <mm@m10e.net>
2019-04-07 00:52:00 +0300

    Initial commit

---
+++ js/content_script.js
@@ -0,0 +1,98 @@
+function walk(rootNode)
+{
+    // Find all the text nodes in rootNode
+    const walker = document.createTreeWalker(
+        rootNode,
+        NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_TEXT,
+        null,
+        false
+    );
+
+    let node;
+
+    // Modify each text node's value
+    while (node = walker.nextNode()) {
+        if (node.nodeType === Node.ELEMENT_NODE) {
+            handleAttrs(node);
+        } else if (node.nodeType === Node.TEXT_NODE) {
+            handleText(node);
+        }
+    }
+}
+
+
+function handleAttrs(node) {
+    if (node.placeholder) {
+        try {
+            node.placeholder = lesivka.encode(node.placeholder);
+        } catch (err) {
+            console.error(err, node.placeholder);
+        }
+    }
+
+    if (node.value) {
+        try {
+            node.value = lesivka.encode(node.value);
+        } catch (err) {
+            console.error(err, node.value);
+        }
+    }
+}
+
+
+function handleText(node) {
+    try {
+        node.nodeValue = lesivka.encode(node.nodeValue);
+    } catch (err) {
+        if (err.message !== "Maximum call stack size exceeded") {
+            console.error(err, node.nodeValue);
+        }
+    }
+}
+
+// The callback used for the document body and title observers
+function observerCallback(mutations) {
+    mutations.forEach(function(mutation) {
+        for (let node of mutation.addedNodes) {
+            if (node.nodeType === Node.TEXT_NODE) {
+                // Replace the text for text nodes
+                handleText(node);
+            } else {
+                // Otherwise, find text nodes within the given node and replace text
+                handleAttrs(node);
+                walk(node);
+            }
+        }
+    });
+}
+
+// Walk the doc (document) body, replace the title, and observe the body and title
+function walkAndObserve(doc) {
+    let docTitle = doc.getElementsByTagName('title')[0],
+    observerConfig = {
+        characterData: true,
+        childList: true,
+        subtree: true
+    },
+    bodyObserver, titleObserver;
+
+    // Do the initial text replacements in the document body and title
+    walk(doc.body);
+    doc.title = lesivka.encode(doc.title);
+
+    // Observe the body so that we replace text in any added/modified nodes
+    bodyObserver = new MutationObserver(observerCallback);
+    bodyObserver.observe(doc.body, observerConfig);
+
+    // Observe the title so we can handle any modifications there
+    if (docTitle) {
+        titleObserver = new MutationObserver(observerCallback);
+        titleObserver.observe(docTitle, observerConfig);
+    }
+}
+
+chrome.storage.sync.get('enabled', function(data) {
+    if (data.enabled) {
+        walkAndObserve(document);
+    }
+});
---
+++ js/popup.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let enableSwitch = document.getElementById('enable');
+    chrome.storage.sync.get('enabled', function(data) {
+        enableSwitch.checked = data.enabled;
+    });
+    enableSwitch.addEventListener('click', function (element) {
+        let enabled = element.target.checked;
+        chrome.storage.sync.set({enabled: enabled});
+    });
+
+    let reloadButton = document.getElementById('reload');
+    reloadButton.addEventListener('click', function () {
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            chrome.tabs.reload(tabs[0].id);
+        });
+    });
+});
